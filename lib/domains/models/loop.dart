ed9fa96a4c27a8455e1baeb9d84a1b3485086634
Johannes Naylor <jonaylor89@gmail.com>
2021-10-07 10:07:16 -0400

    feat(testing) : Loop Model unit testing (#30)

--- lib/domains/models/loop.dart
+++ lib/domains/models/loop.dart
@@ -1,62 +1,76 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
 import 'package:intheloopapp/utils.dart';
 import 'package:json_annotation/json_annotation.dart';
 
 part 'loop.g.dart';
 
-/// Deserialize Firebase Timestamp data type from Firestore
-Timestamp firestoreTimestampFromJson(dynamic value) {
-  return value != null ? Timestamp.fromMicrosecondsSinceEpoch(value) : value;
-}
-
-/// This method only stores the "timestamp" data type back in the Firestore
-dynamic firestoreTimestampToJson(Timestamp? value) =>
-    value != null ? value.microsecondsSinceEpoch : null;
-
 @JsonSerializable()
-class Loop {
-  String id;
-  String? userId;
-  String? title;
-  String? audio;
-
-  @JsonKey(
-    fromJson: firestoreTimestampFromJson,
-    toJson: firestoreTimestampToJson,
-  )
-  Timestamp? timestamp;
-
-  int? likes;
-  int? downloads;
-  int? comments;
-  List<String>? tags;
-  bool? deleted;
+class Loop extends Equatable {
+  final String id;
+  final String userId;
+  final String title;
+  final String audio;
+  final DateTime timestamp;
+  final int likes;
+  final int downloads;
+  final int comments;
+  final List<String> tags;
+  final bool deleted;
 
   Loop({
-    this.id = '',
-    this.userId = '',
-    this.title = '',
-    this.audio = '',
-    this.timestamp,
-    this.likes = 0,
-    this.downloads = 0,
-    this.comments = 0,
-    this.tags = const [],
-    this.deleted = false,
+    required this.id,
+    required this.userId,
+    required this.title,
+    required this.audio,
+    required this.timestamp,
+    required this.likes,
+    required this.downloads,
+    required this.comments,
+    required this.tags,
+    required this.deleted,
   });
+
+  @override
+  List<Object> get props => [
+        id,
+        userId,
+        title,
+        audio,
+        likes,
+        downloads,
+        comments,
+        tags,
+        deleted,
+      ];
+
+  static Loop get empty => Loop(
+        id: '',
+        userId: '',
+        title: '',
+        audio: '',
+        timestamp: DateTime.now(),
+        likes: 0,
+        downloads: 0,
+        comments: 0,
+        tags: const [],
+        deleted: false,
+      );
 
   factory Loop.fromJson(Map<String, dynamic> json) => _$LoopFromJson(json);
   Map<String, dynamic> toJson() => _$LoopToJson(this);
 
   factory Loop.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
+    final Timestamp tmpTimestamp = doc.getOrElse('timestamp', Timestamp.now());
+
     return Loop(
       id: doc.id,
-      userId: doc['userId'] ?? '',
-      title: doc['title'] ?? '',
-      audio: doc['audio'] ?? '',
-      timestamp: doc['timestamp'] ?? null,
-      likes: doc['likes'] ?? 0,
-      downloads: doc['downloads'] ?? 0,
+      userId: doc.getOrElse('userId', ''),
+      title: doc.getOrElse('title', ''),
+      audio: doc.getOrElse('audio', ''),
+      timestamp: tmpTimestamp.toDate(),
+      likes: doc.getOrElse('likes', 0),
+      downloads: doc.getOrElse('downloads', 0),
       comments: doc.getOrElse('comments', 0),
       tags: List.from(
         doc.getOrElse('tags', []),
