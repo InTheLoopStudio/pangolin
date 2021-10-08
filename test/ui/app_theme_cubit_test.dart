dd7b5b1da49b43dc9288266ef4beefb01d36cdde
Johannes Naylor <jonaylor89@gmail.com>
2021-10-07 20:40:40 -0400

    feat(bloc testing): AppThemeCubit testing

---
+++ test/ui/app_theme_cubit_test.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:intheloopapp/ui/app_theme_cubit.dart';
+import 'package:path_provider/path_provider.dart';
+
+void main() {
+  setUp(() async {
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: kIsWeb
+        ? HydratedStorage.webStorageDirectory
+        : await getTemporaryDirectory(),
+  );
+
+
+  });
+
+  group('AppThemeCubit', () {
+
+    blocTest(
+      'emit `true` when theme updated to dark',
+      build: () => AppThemeCubit(),
+      expect: () => [], 
+    );
+
+    blocTest(
+      'emit `true` when theme updated to dark',
+      build: () => AppThemeCubit(),
+      act: (AppThemeCubit bloc) => bloc.updateTheme(true),
+      expect: () => [true], 
+    );
+
+    blocTest(
+      'emit `false` when theme updated to light',
+      build: () => AppThemeCubit(),
+      act: (AppThemeCubit bloc) => bloc.updateTheme(false),
+      expect: () => [false], 
+    );
+  });
+}
