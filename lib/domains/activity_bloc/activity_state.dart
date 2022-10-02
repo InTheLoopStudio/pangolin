951e41ee098782bba1b56b77ef0dce4762ccabbe
Johannes Naylor <jonaylor89@gmail.com>
2022-10-02 16:43:05 -0400

    refactor

--- lib/domains/activity_bloc/activity_state.dart
+++ lib/domains/activity_bloc/activity_state.dart
@@ -1,27 +1,40 @@
 part of 'activity_bloc.dart';
 
+/// The state for activities aka notifications
 abstract class ActivityState extends Equatable {
+  /// Initially, activies is an empty list
   const ActivityState({this.activities = const []});
 
+  /// The activities or notifications on the app
   final List<Activity> activities;
 
   @override
   List<Object> get props => [activities];
 }
 
+/// The initial state of activities
 class ActivityInitial extends ActivityState {
+  /// The initial state of activities is to have no activities
+  /// i.e. an empty list
   const ActivityInitial() : super(activities: const []);
 }
 
+/// The state after successfully pulling new activities
 class ActivitySuccess extends ActivityState {
-  const ActivitySuccess({activities = const []})
+  /// By default, the activity list is empty
+  const ActivitySuccess({List<Activity> activities = const <Activity>[]})
       : super(activities: activities);
 }
 
+/// The state when reaching the last page in paginated activities
 class ActivityEnd extends ActivityState {
-  const ActivityEnd({activities = const []}) : super(activities: activities);
+  /// By default, the activity list is empty
+  const ActivityEnd({List<Activity> activities = const []})
+      : super(activities: activities);
 }
 
+/// The state when there is an error pulling new activities
 class ActivityFailure extends ActivityState {
+  /// By default, the activity list is empty
   const ActivityFailure() : super(activities: const []);
 }
