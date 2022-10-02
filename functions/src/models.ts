dccf509de2e4dad4ec86d1f485fd3c74e01c90ea
Johannes Naylor <jonaylor89@gmail.com>
2022-10-02 15:20:46 -0400

    add models

--- functions/src/models.ts
+++ functions/src/models.ts
@@ -1,11 +1,49 @@
+
+type AccountType = "free" | "venue"
+
 type UserModel = {
-    id: string;
-}
+  id: string;
+  email?: string;
+  username?: string;
+  bio?: string;
+  profilePicture?: string;
+  location?: string;
+  onboarded?: boolean;
+  loopsCount?: number;
+  badgesCount?: number;
+  deleted?: boolean;
+  shadowBanned?: boolean;
+  accountType?: AccountType;
+  twitterHandle?: string;
+  instagramHandle?: string;
+  tiktokHandle?: string;
+  soundcloudHandle?: string;
+  youtubeChannelId?: string;
+  pushNotificationsLikes?: boolean;
+  pushNotificationsComments?: boolean;
+  pushNotificationsFollows?: boolean;
+  pushNotificationsDirectMessages?: boolean;
+  pushNotificationsITLUpdates?: boolean;
+  emailNotificationsAppReleases?: boolean;
+  emailNotificationsITLUpdates?: boolean;
+};
 
 type Badge = {
-    id: string;
-}
+  id: string;
+  senderId: string;
+  receiverId: string;
+  imageUrl: string;
+};
 
 type Loop = {
-    id: string;
-}
+  id: string;
+  userId: string;
+  title: string;
+  audio: string;
+  likes: number;
+  downloads: number;
+  comments: number;
+  shares: number;
+  tags: Array<string>;
+  deleted: boolean;
+};
