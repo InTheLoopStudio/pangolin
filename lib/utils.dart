72c021a1c33de61f2495f2374bc7772e3b2fb393
Johannes Naylor <jonaylor89@gmail.com>
2022-10-08 07:53:35 -0300

    add documentation

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,7 +1,14 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
+/// project extensions for firebase's `DocumentSnapshot<T>` class
 extension DefaultValue<K, V> on DocumentSnapshot<Map<String, dynamic>> {
+  /// helper function to try an index the `DocumentSnapshot<T>` object
+  /// and return a custom default value if the desired index doesn't exist
+  /// 
+  /// This is useful for adding new fields to DB models since it means
+  /// a custom migration script doesn't need to be made every time
+  /// and can instead just set its default client-side
   V getOrElse(K key, V defaultValue) {
     if (data() != null && data()!.containsKey(key)) {
       return (data()![key] ?? defaultValue) as V;
@@ -11,12 +18,16 @@ extension DefaultValue<K, V> on DocumentSnapshot<Map<String, dynamic>> {
   }
 }
 
+/// helper function to format `DateTime` objects
 String formatDate(DateTime date) {
   final dateFormat = DateFormat.yMd().add_jm();
 
   return dateFormat.format(date);
 }
 
+/// helper function to format `DateTime` objects into string
+/// specifically for `DateTime`s that are within 7 days of 
+/// `DateTime.now()`
 String formatDateSameWeek(DateTime date) {
   DateFormat dateFormat;
 
@@ -29,12 +40,15 @@ String formatDateSameWeek(DateTime date) {
   return dateFormat.format(date);
 }
 
+/// helper function to format `DateTime` objects into strings
 String formatDateMessage(DateTime date) {
   final dateFormat = DateFormat('EEE. MMM. d ' 'yy' ' hh:mm a');
 
   return dateFormat.format(date);
 }
 
+/// helper function to determine if a given 
+/// timestamp is within a week of `DateTime.now()`
 bool isSameWeek(DateTime timestamp) {
   return DateTime.now().difference(timestamp).inDays < 7;
 }
