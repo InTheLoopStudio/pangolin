d16d06ca79b4d2cb60fcd3c9a2da3bbaf42d9185
Johannes Naylor <jonaylor89@gmail.com>
2022-11-28 12:12:58 +0100

    cubit and state

--- lib/ui/views/post_feed/post_feed_cubit.dart
+++ lib/ui/views/post_feed/post_feed_cubit.dart
@@ -1,8 +1,89 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:intheloopapp/data/database_repository.dart';
+import 'package:intheloopapp/domains/models/post.dart';
 
 part 'post_feed_state.dart';
 
 class PostFeedCubit extends Cubit<PostFeedState> {
-  PostFeedCubit() : super(PostFeedInitial());
+  PostFeedCubit({
+    required this.currentUserId,
+    required this.databaseRepository,
+  }) : super(const PostFeedState());
+
+  final String currentUserId;
+  final DatabaseRepository databaseRepository;
+  StreamSubscription<Post>? postListener;
+
+  Future<void> initPosts({bool clearPosts = true}) async {
+    await postListener?.cancel();
+    if (clearPosts) {
+      emit(
+        state.copyWith(
+          status: PostFeedStatus.initial,
+          posts: [],
+          hasReachedMax: false,
+        ),
+      );
+    }
+
+    final followingPosts = await databaseRepository.getFollowingPosts(
+      currentUserId,
+      limit: 1,
+    );
+    if (!followingPosts.isNotEmpty) {
+      emit(state.copyWith(status: PostFeedStatus.success));
+    }
+
+    postListener = databaseRepository
+        .followingPostsObserver(currentUserId)
+        .listen((Post event) {
+      // print('post { ${event.id} : ${event.title} }');
+      emit(
+        state.copyWith(
+          status: PostFeedStatus.success,
+          posts: List.of(state.posts)..add(event),
+        ),
+      );
+    });
+  }
+
+  Future<void> fetchMorePosts() async {
+    if (state.hasReachedMax) return;
+
+    try {
+      if (state.status == PostFeedStatus.initial) {
+        await initPosts();
+      }
+
+      final posts = await databaseRepository.getFollowingPosts(
+        currentUserId,
+        lastPostId: state.posts.last.id,
+      );
+      posts.isEmpty
+          ? emit(state.copyWith(hasReachedMax: true))
+          : emit(
+              state.copyWith(
+                status: PostFeedStatus.success,
+                posts: List.of(state.posts)..addAll(posts),
+                hasReachedMax: false,
+              ),
+            );
+    } on Exception {
+      emit(state.copyWith(status: PostFeedStatus.failure));
+    }
+  }
+
+  void tapEasterEgg() {
+    emit(state.copyWith(easterEggTapped: state.easterEggTapped + 1));
+  }
+
+  @override
+  Future<void> close() async {
+    await postListener?.cancel();
+    await super.close();
+  }
 }
--- lib/ui/views/post_feed/post_feed_state.dart
+++ lib/ui/views/post_feed/post_feed_state.dart
@@ -1,10 +1,45 @@
 part of 'post_feed_cubit.dart';
 
-abstract class PostFeedState extends Equatable {
-  const PostFeedState();
+enum PostFeedStatus { initial, success, failure }
+
+@immutable
+class PostFeedState extends Equatable {
+  const PostFeedState({
+    this.currentIndex = 0,
+    this.hasReachedMax = false,
+    this.status = PostFeedStatus.initial,
+    this.posts = const [],
+    this.easterEggTapped = 0,
+  });
+
+  final int currentIndex;
+  final bool hasReachedMax;
+  final PostFeedStatus status;
+  final List<Post> posts;
+  final int easterEggTapped;
 
   @override
-  List<Object> get props => [];
-}
+  List<Object> get props => [
+        currentIndex,
+        hasReachedMax,
+        status,
+        posts,
+        easterEggTapped,
+      ];
 
-class PostFeedInitial extends PostFeedState {}
+  PostFeedState copyWith({
+    int? currentIndex,
+    bool? hasReachedMax,
+    PostFeedStatus? status,
+    List<Post>? posts,
+    int? easterEggTapped,
+  }) {
+    return PostFeedState(
+      currentIndex: currentIndex ?? this.currentIndex,
+      hasReachedMax: hasReachedMax ?? this.hasReachedMax,
+      status: status ?? this.status,
+      posts: posts ?? this.posts,
+      easterEggTapped: easterEggTapped ?? this.easterEggTapped,
+    );
+  }
+}
