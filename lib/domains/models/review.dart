f8c893ae9c5bc88a8b8d2325a14765bda1b43a9c
Johannes Naylor <jonaylor89@gmail.com>
2023-04-12 15:59:46 +0200

    add review thangs

--- lib/domains/models/review.dart
+++ lib/domains/models/review.dart
@@ -7,6 +7,10 @@ class Review extends Equatable {
     required this.bookingId,
     required this.reviewerId,
     required this.revieweeId,
+    required this.overallRating,
+    required this.fanEngagementRating,
+    required this.onTime,
+    required this.comments,
   });
 
   factory Review.fromDoc(DocumentSnapshot<Map<String, dynamic>> doc) {
@@ -14,18 +18,30 @@ class Review extends Equatable {
       bookingId: doc.getOrElse('bookingId', '') as String,
       reviewerId: doc.getOrElse('reviewerId', '') as String,
       revieweeId: doc.getOrElse('revieweeId', '') as String,
+      overallRating: doc.getOrElse('overallRating', 0.0) as double,
+      fanEngagementRating: doc.getOrElse('fanEngagementRating', 0.0) as double,
+      onTime: doc.getOrElse('onTime', true) as bool,
+      comments: doc.getOrElse('comments', '') as String,
     );
   }
 
   final String bookingId;
   final String reviewerId;
   final String revieweeId;
+  final double overallRating;
+  final double fanEngagementRating;
+  final bool onTime;
+  final String comments;
 
   @override
   List<Object> get props => [
         bookingId,
         reviewerId,
         revieweeId,
+        overallRating,
+        fanEngagementRating,
+        onTime,
+        comments,
       ];
 
   Map<String, dynamic> toMap() {
@@ -33,6 +49,10 @@ class Review extends Equatable {
       'bookingId': bookingId,
       'reviewerId': reviewerId,
       'revieweeId': revieweeId,
+      'overallRating': overallRating,
+      'fanEngagementRating': fanEngagementRating,
+      'onTime': onTime,
+      'comments': comments,
     };
   }
 }
