51b8c48d3b258d886d1a45f0a70ed6e93fad7e50
Johannes Naylor <jonaylor89@gmail.com>
2023-04-15 20:45:10 +0200

    fix request to book button

--- lib/ui/widgets/profile_view/request_to_book.dart
+++ lib/ui/widgets/profile_view/request_to_book.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:intheloopapp/data/payment_repository.dart';
+import 'package:intheloopapp/domains/models/payment_user.dart';
 import 'package:intheloopapp/domains/navigation_bloc/navigation_bloc.dart';
 import 'package:intheloopapp/ui/views/profile/profile_cubit.dart';
+import 'package:skeleton_animation/skeleton_animation.dart';
 
 class RequestToBookButton extends StatelessWidget {
   const RequestToBookButton({super.key});
@@ -9,30 +12,53 @@ class RequestToBookButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final navigationBloc = context.read<NavigationBloc>();
+    final payments = context.read<PaymentRepository>();
     return BlocBuilder<ProfileCubit, ProfileState>(
       builder: (context, state) {
         return (state.currentUser.id != state.visitedUser.id &&
                 state.currentUser.isVenue)
             ? Padding(
                 padding: const EdgeInsets.symmetric(vertical: 8),
-                child: FilledButton(
-                  onPressed: state
-                          .visitedUser.stripeConnectedAccountId.isNotEmpty
-                      ? () async {
-                          navigationBloc.add(
-                            PushCreateBooking(
-                              requesteeId: state.visitedUser.id,
-                              requesteeStripeConnectedAccountId:
-                                  state.visitedUser.stripeConnectedAccountId,
-                              requesteeBookingRate:
-                                  state.visitedUser.bookingRate,
-                            ),
-                          );
-                        }
-                      : null,
-                  child: state.visitedUser.stripeConnectedAccountId.isNotEmpty
-                      ? const Text('Request to Book')
-                      : const Text('Artist Payment Info not Connected'),
+                child: FutureBuilder<PaymentUser?>(
+                  future: () async {
+                    if (state.visitedUser.stripeConnectedAccountId.isEmpty) {
+                      return null;
+                    }
+
+                    return payments.getAccountById(
+                      state.visitedUser.stripeConnectedAccountId,
+                    );
+                  }(),
+                  builder: (context, snapshot) {
+                    if (!snapshot.hasData) {
+                      return const SkeletonText(height: 40);
+                    }
+
+                    final paymentUser = snapshot.data;
+
+                    final enabled = paymentUser != null &&
+                        paymentUser.payoutsEnabled &&
+                        state.visitedUser.stripeConnectedAccountId.isNotEmpty;
+
+                    return FilledButton(
+                      onPressed: enabled
+                          ? () {
+                              navigationBloc.add(
+                                PushCreateBooking(
+                                  requesteeId: state.visitedUser.id,
+                                  requesteeStripeConnectedAccountId: state
+                                      .visitedUser.stripeConnectedAccountId,
+                                  requesteeBookingRate:
+                                      state.visitedUser.bookingRate,
+                                ),
+                              );
+                            }
+                          : null,
+                      child: !enabled
+                          ? const Text('Artist Payment Info not Connected')
+                          : const Text('Request to Book'),
+                    );
+                  },
                 ),
               )
             : const SizedBox(
